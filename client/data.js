2a70814ff5418134c09471e02bcb4c46583bc6e4
Neil-140301 <alvares.1@iitj.ac.in>
2021-11-01 16:18:57 +0530

    edit | added local images

--- client/data.js
+++ client/data.js
@@ -1,21 +1,21 @@
 export const sliderItems = [
   {
     id: 1,
-    img: 'https://i.ibb.co/XsdmR2c/1.png',
+    img: '/7.webp',
     title: 'SUMMER SALE',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: 'f5fafd',
   },
   {
     id: 2,
-    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    img: '/2.png',
     title: 'AUTUMN COLLECTION',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: 'fcf1ed',
   },
   {
     id: 3,
-    img: 'https://i.ibb.co/cXFnLLV/3.png',
+    img: '/3.png',
     title: 'LOUNGEWEAR LOVE',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
     bg: 'fbf0f4',
@@ -25,19 +25,19 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    img: '/6.jpeg',
     title: 'SHIRT STYLE!',
     category: 'women',
   },
   {
     id: 2,
-    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    img: '/4.jpeg',
     title: 'LOUNGEWEAR LOVE',
     category: 'coats',
   },
   {
     id: 3,
-    img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+    img: '/5.jpeg',
     title: 'LIGHT JACKETS',
     category: 'jeans',
   },
